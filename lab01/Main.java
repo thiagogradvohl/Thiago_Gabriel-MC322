24638bae0b9a39d3de86b827a58306fca3f2e382
Gabriel Lopes de Barros <ra281198@sepultura.lab.ic.unicamp.br>
2025-03-13 15:30:46 -0300

    ajustes no código

---
+++ lab01/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Robo robo = new Robo(0, 0, "CR7");
+        Ambiente ambiente = new Ambiente(50, 50);
+
+        robo.exibirPosicao();
+
+        robo.mover(10, 10);
+        
+        System.out.printf("Está dentro do limite %b\n", ambiente.dentroDosLimites(20, 20));
+        
+        robo.exibirPosicao();
+    }
+}
