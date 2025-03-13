d875e3dc1baeef9a54b8ac4447083bf590413fdd
gabriellopesdebarros <g281198@dac.unicamp.br>
2025-03-13 15:59:48 -0300

    Update Robo.java

--- lab01/Robo.java
+++ lab01/Robo.java
@@ -12,23 +12,28 @@ public class Robo
     }
 
     public String getNome() {
+        //retorna o nome do robô.
         return nome;
     }
 
     public int getPosicaox() {
+        //retorna a posição x do robô.
         return posicaox;
     }
 
     public int getPosicaoy() {
+        //retorna a posição y do robô.
         return posicaoy;
     }
 
     public void mover(int deltaX, int deltaY) {
+        //move as posições x e y do robô de acordo com um delta x e um delta y.
         this.posicaox += deltaX;
         this.posicaoy += deltaY; 
     }
     
     public void exibirPosicao() {
+        //imprime as posições x e y do robô.
         System.out.printf("Posicao X: %d\nPosicao Y: %d\n", posicaox, posicaoy);
     }
 }
