56e7e27e23c08663f2dd33bd5bf2b4af7d20dd71
thiagogradvohl <163688468+thiagogradvohl@users.noreply.github.com>
2025-03-13 15:54:21 -0300

    Ambiente.java documentado

--- lab01/Ambiente.java
+++ lab01/Ambiente.java
@@ -3,19 +3,23 @@ public class Ambiente {
     private int largura;
 
     public Ambiente(int altura, int largura) {
+        //Método construtor: Define a altura e a largura do ambiente de movimentação do robô.
         this.altura = altura;
         this.largura = largura;
     }
-
+    
     public int getAltura() {
+        //retorna a altura do ambiente de movimentação do robô.
         return altura;
     }
 
     public int getLargura() {
+        //retorna a largura do ambiente de movimentação do robô.
         return largura;
     }
 
     public boolean dentroDosLimites(int x, int y) {
+        //retorna se o robô está, ou não, nos limites do ambiente.
         return x <= this.largura && x >= 0 && y >= 0 && y <= this.altura;
     }
 }
