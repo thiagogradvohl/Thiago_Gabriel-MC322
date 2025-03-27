e867ce489f4dd8f39dccd734fde4105c669af348
thiagogradvohl <163688468+thiagogradvohl@users.noreply.github.com>
2025-03-27 16:08:37 -0300

    Update RoboTerrestre.java

--- lab02/RoboTerrestre.java
+++ lab02/RoboTerrestre.java
@@ -1,25 +1,32 @@
 public class RoboTerrestre extends Robo {
     private int velocidadeMaxima;
+    private int velocidade;
     
-    public RoboTerrestre(int posicaox, int posicaoy, String nome, int velocidadeMaxima) {
+    public RoboTerrestre(int posicaox, int posicaoy, String nome, int velocidadeMaxima, int velocidade) {
         super(posicaox, posicaoy, nome);
         this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidade=velociade.
     }
 
+    
     public int getVelocidadeMaxima() {
         return velocidadeMaxima;    
     }
+    
+    public int getVelocidade() {
+        return velocidade;
+    }
 
-    private int velocidade(int delta_x, int delta_y) {
-        //calcula a velocidade do movimento (em m/s), considerando este como sendo realizado em 1 segundo
-        return (int) Math.sqrt(Math.pow(delta_x, 2) + Math.pow(delta_y, 2));
+    
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
     }
 
     @Override
     public void mover(int delta_x, int delta_y) {
         //metodo sobrescrito do Robo, move o robo se a sua velocidade for menor que a velocidade maxima
         //para o calculo da velocidade considerou-se o tempo do movimento como sendo de 1 segundo
-        if (velocidade(delta_x, delta_y) > this.velocidadeMaxima)
+        if (getVelocidade() > this.velocidadeMaxima)
             System.out.printf("Não foi possível realizar o movimento. A velocidade está acima da velocidade máxima.\n");
         else {
             this.posicaox += delta_x;   
