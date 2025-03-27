a61dc84d9590ec752b21ff64dc6fa4f5e478b1c6
thiagogradvohl <163688468+thiagogradvohl@users.noreply.github.com>
2025-03-27 15:51:39 -0300

    Create Obstaculo.java
    Classe obstaculo criada

---
+++ lab02/Obstaculo.java
@@ -0,0 +1,34 @@
+public class Obstaculo
+{
+    private String nome_obstaculo;
+    private int pos_x;
+    private int pos_y;
+    
+    public Obstaculo(int pos_x, int pos_y, String nome_obstaculo){
+        //Método construtor: Define o nome, a posicao x e a posicao y e a posicao z do obstaculo.
+        this.nome_obstaculo = nome_obstaculo;
+        this.pos_x = pos_x;
+        this.pos_y=pos_y;
+    }
+    public String getNome() {
+        //retorna o nome do Obstaculo.
+        return nome_obstaculo;
+    }
+
+    public int getPosicaox() {
+        //retorna a posição x do Obstaculo.
+        return pos_x;
+    }
+
+    public int getPosicaoy() {
+        //retorna a posição y do Obstaculo.
+        return pos_y;
+    }
+   
+    
+    public void exibirPosicao() {
+        //imprime as posições x e y do Obstaculo.
+        System.out.printf("Posicao X: %d\nPosicao Y: %d\n", pos_x, pos_y);
+    }
+    
+}
