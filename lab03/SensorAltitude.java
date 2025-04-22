812f2f4a40154dbc51e881262f876ebd23887fc3
Gabriel Lopes de Barros <g281198@dac.unicamp.br>
2025-04-22 17:04:32 -0300

    Robo deixa de ser atributo da classe SensorAltitude e passa a ser parametro da funcao monitorar

--- lab03/SensorAltitude.java
+++ lab03/SensorAltitude.java
@@ -2,35 +2,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SensorAltitude extends SensorProximidade {
-    private RoboAereo robo_aereo;
-    
 
-    public SensorAltitude(double raio, RoboAereo robo_aereo, Ambiente ambiente_robos) {
-        super(raio, robo_aereo,ambiente_robos);
-        this.robo_aereo = robo_aereo;
+    public SensorAltitude(double raio, Ambiente ambiente_robos) {
+        super(raio, ambiente_robos);
     }
 
 
-    public List<int[]> identificarObstaculosAereos(){
+    public List<int[]> identificarObstaculosAereos(RoboAereo robo_aereo){
         
         
         List<int[]> obstaculos_vizinhos = new ArrayList<>();
 
         for(Obstaculo obs : this.ambiente_robos.obstaculos)
         {
-            if(obs.getPosicao_x1() == this.robo_aereo.getPosicaox() && obs.getPosicao_y1() == this.robo_aereo.getPosicaoy() && obs.getAltura() == this.robo_aereo.getAltitude())
+            if(obs.getPosicao_x1() == robo_aereo.getPosicaox() && obs.getPosicao_y1() == robo_aereo.getPosicaoy() && obs.getAltura() == robo_aereo.getAltitude())
             {
                 System.out.printf("Robô colidiu com um obstáculo!\nPosição da colisão: (%d,%d,%d)",obs.getPosicao_x2(),obs.getPosicao_y2(),obs.getAltura());
             }
-            else if(obs.getPosicao_x1() == this.robo_aereo.getPosicaox()+1 || obs.getPosicao_x1() == this.robo_aereo.getPosicaox()-1)
+            else if(obs.getPosicao_x1() == robo_aereo.getPosicaox()+1 || obs.getPosicao_x1() == robo_aereo.getPosicaox()-1)
             {
                 obstaculos_vizinhos.add(new int[]{obs.getPosicao_x1(),obs.getPosicao_y1(),obs.getAltura()});
             }
-            else if(obs.getPosicao_y1() == this.robo_aereo.getPosicaoy()+1 || obs.getPosicao_y1() == this.robo_aereo.getPosicaoy()-1)
+            else if(obs.getPosicao_y1() == robo_aereo.getPosicaoy()+1 || obs.getPosicao_y1() == robo_aereo.getPosicaoy()-1)
             {
                 obstaculos_vizinhos.add(new int[]{obs.getPosicao_x1(),obs.getPosicao_y1(),obs.getAltura()});
             }
-            else if (obs.getAltura() == this.robo_aereo.getAltitude()+1 || obs.getAltura() == this.robo_aereo.getAltitude()-1)
+            else if (obs.getAltura() == robo_aereo.getAltitude()+1 || obs.getAltura() == robo_aereo.getAltitude()-1)
             {
                 obstaculos_vizinhos.add(new int[]{obs.getPosicao_x1(),obs.getPosicao_y1(),obs.getAltura()});
             }
@@ -48,9 +45,8 @@ public class SensorAltitude extends SensorProximidade {
         }
     }
 
-    @Override
-    public void monitorar(String contexto) {
-        List<int[]> proximos = identificarObstaculosAereos();
+    public void monitorar(RoboAereo robo_aereo) {
+        List<int[]> proximos = identificarObstaculosAereos(robo_aereo);
         exibirObstaculos(proximos);
     }
 
