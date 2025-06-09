d3dd8347491da2df3a27baa970b9d305de22a795
gabriellopesdebarros <g281198@dac.unicamp.br>
2025-06-09 20:57:47 -0300

    Delete lab05/DestruidorAutonomo.java

--- lab05/DestruidorAutonomo.java
+++
@@ -1,6 +0,0 @@
-public interface DestruidorAutonomo{
-   void atualizar_ambiente(Ambiente ambiente);
-   SensorProximidade identificar_sp();
-   void recarregarBateria(int energia_adicionada);
-   void atualizar_entidades_proximas(Ambiente a);
-}
