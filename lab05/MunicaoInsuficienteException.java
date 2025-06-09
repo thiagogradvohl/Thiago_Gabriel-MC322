737dccf05fbdd382092fabb35293f00f688c2c9a
gabriellopesdebarros <g281198@dac.unicamp.br>
2025-06-09 20:59:44 -0300

    Delete lab05/MunicaoInsuficienteException.java

--- lab05/MunicaoInsuficienteException.java
+++
@@ -1,5 +0,0 @@
-public class MunicaoInsuficienteException extends Exception {
-    public MunicaoInsuficienteException() {
-        super("Municao insuficiente para a acao. A quantidade minima eh 10.");
-    }
-}
