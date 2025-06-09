aa75b0d115da819d2b6a3e9b4dcf266adca8ca38
gabriellopesdebarros <g281198@dac.unicamp.br>
2025-06-09 20:57:40 -0300

    Delete lab05/Comunicavel.java

--- lab05/Comunicavel.java
+++
@@ -1,4 +0,0 @@
-public interface Comunicavel {
-    String enviarMensagem(Comunicavel destinatario, String mensagem) throws RoboDesligadoException;
-    void receberMensagem(String mensagem) throws RoboDesligadoException;  //pode lancar robodesligadoexception
-}
